8776bd98ad31a3db8cf0f4fafdf27d0b40f85bed
Connor <connor@localhost.localdomain>
2022-03-22 17:43:26 -0400

    Set up basic express and .env structure. Further coding in seperate branches

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
-// Just putting something in for initial commit
-console.log('Hello World!')
+/**
+ * To run with nodemon, call npm start
+ * Must have a .env file created to run - .env not shared
+ */
+
+// Basic Dependencies
+const dotenv = require('dotenv').config();
+const express = require('express');
+
+// Setting app (express)
+const app = express();
+
+// Setting to use express.json
+app.use(express.json());
+
+// Listening on the .env defined port, and display 
+app.listen(dotenv.parsed.PORT, () => {
+    console.log(`Listening on port: ${dotenv.parsed.PORT}`)
+})
