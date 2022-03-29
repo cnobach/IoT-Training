8eb02e7bf17f7ed22e63323b411ad9d6ff072fb6
Connor Nobach <clnobach@gmail.com>
2022-03-29 15:24:53 -0400

    Added environment variables in environment to make more secure.

--- Frontend/src/app/pages/login/services/server.service.ts
+++ Frontend/src/app/pages/login/services/server.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,10 @@ export class ServerService {
 
   constructor(private http: HttpClient) { }
 
+  backend_url = environment.backendServer;
+  backend_port = environment.backendPort;
+
   login(user : any): Observable<any>{
-    return this.http.post('http://localhost:8080/login', user);
+    return this.http.post(`${this.backend_url}:${this.backend_port}/login`, user);
   }
 }
--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  backendServer: 'http://localhost',
+  backendPort: '8080'
 };
 
 /*
