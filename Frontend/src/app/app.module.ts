1ee07d1a63c8db6141aa4583298617b4f2a66a44
Connor Nobach <clnobach@gmail.com>
2022-03-31 15:29:10 -0400

    Added router import.

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,7 +33,8 @@ import { MessageModule } from 'primeng/message';
     ButtonModule,
     RippleModule,
     MessagesModule,
-    MessageModule
+    MessageModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
