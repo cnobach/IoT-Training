467f22dedb5766fcaa6f365c533e2884f896ea49
Connor Nobach <clnobach@gmail.com>
2022-05-12 14:11:37 -0400

    Completed the transaction test.

--- Backend/test/trans.test.js
+++ Backend/test/trans.test.js
@@ -18,3 +18,30 @@ beforeAll((done) => {
             done();
         })
 })
+
+describe('Creating a new transaction', () => {
+
+    test('PUT /trans/new', async() => {
+
+        let trans = {
+            items: [5, 6, 7, 8],
+            id: 1
+        }
+
+        await agent.put('/trans/new')
+            .expect(200)
+            .set('token', cookie)
+            .send(trans)
+            .then((res) => {
+                let test = JSON.parse(res.text);
+                console.log('RES:', test[0]);
+                let date = new Date();
+
+                expect(test[0].items).toEqual(trans.items);
+                expect(test[0].customer).toEqual(trans.id);
+
+            })
+
+    })
+
+})
